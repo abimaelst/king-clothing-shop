9f450799ed13376dbb3fa6e94af0b09b6894a025
abimael.tavares <abimael.tavares@devsquad.com>
2023-08-20 11:22:16 -0300

    adding prettier and some cards

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,43 @@
-import Image from 'next/image'
+import Image from 'next/image';
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     <h1>Starting...</h1>
+      <div>
+        {/*<img src="" alt=""/>*/}
+        <div>
+          <h2>Hats</h2>
+          <p>Show Now</p>
+        </div>
+      </div>
+      <div>
+        {/*<img src="" alt=""/>*/}
+        <div>
+          <h2>Jackets</h2>
+          <p>Show Now</p>
+        </div>
+      </div>
+      <div>
+        {/*<img src="" alt=""/>*/}
+        <div>
+          <h2>Sneakers</h2>
+          <p>Show Now</p>
+        </div>
+      </div>
+      <div>
+        {/*<img src="" alt=""/>*/}
+        <div>
+          <h2>Women&apos;s</h2>
+          <p>Show Now</p>
+        </div>
+      </div>
+      <div>
+        {/*<img src="" alt=""/>*/}
+        <div>
+          <h2>Mens</h2>
+          <p>Show Now</p>
+        </div>
+      </div>
     </main>
-  )
+  );
 }
