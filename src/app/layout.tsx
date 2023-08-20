837cd244cbd180af1c04f6ff379dd9cf0cf7eae3
abimael.tavares <abimael.tavares@devsquad.com>
2023-08-20 18:05:52 -0300

    changing font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,22 @@
-import '../../assets/css/globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import '../../assets/css/globals.css';
+import type { Metadata } from 'next';
+import { Open_Sans } from 'next/font/google';
 
-const inter = Inter({ subsets: ['latin'] })
+const openSans = Open_Sans({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'King Clothing',
   description: 'The besting Clothing shop',
-}
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={openSans.className}>{children}</body>
     </html>
-  )
+  );
 }
